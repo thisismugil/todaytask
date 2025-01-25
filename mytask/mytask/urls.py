cfc33af38246e8815c52fb6c0108cff0e54edc2d
thisismugil <thinknewbro@gmail.com>
2025-01-25 16:20:12 +0530

    Add student API URL routing

--- mytask/mytask/urls.py
+++ mytask/mytask/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/instructor/',include('instructor.urls'))
+    path('api/instructor/',include('instructor.urls')),
+    path('api/student/',include('student.urls')),
 ]
