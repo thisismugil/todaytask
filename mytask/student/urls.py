3484dc91a9e2fa2e90a3fc7c2bec776ecc1a20e5
Karthik-ihub <karthik.p.ihub@sngroups.com>
2025-01-25 14:41:24 +0530

    Updated the code for Student login,registering and forgot password

---
+++ mytask/student/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    register_user,
+    verify_email_otp,
+    login_user,
+    forgot_password_request,
+    verify_reset_otp,
+    reset_password
+)
+
+urlpatterns = [
+    path('register/', register_user, name='register_user'),
+    path('verify-email/', verify_email_otp, name='verify_email_otp'),
+    path('login/', login_user, name='login_user'),
+    path('forgot-password/', forgot_password_request, name='forgot_password_request'),
+    path('verify-reset-otp/', verify_reset_otp, name='verify_reset_otp'),
+    path('reset-password/', reset_password, name='reset_password'),
+]
